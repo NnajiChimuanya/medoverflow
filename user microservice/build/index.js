481c64484d7f1aa3512bb507a7c5f897f3c65ee0
Chimuanya Nnaji <nnajichimuanya50@gmail.com>
2022-09-26 19:25:17 +0400

    Initialized this project

---
+++ user microservice/build/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cluster_1 = __importDefault(require("cluster"));
+const os_1 = __importDefault(require("os"));
+const worker_threads_1 = require("worker_threads");
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+if (cluster_1.default.isMaster) {
+    const cpus = os_1.default.cpus().length;
+    console.log(`Forking for ${cpus}`);
+    for (let i = 0; i < cpus; i++) {
+        cluster_1.default.fork();
+    }
+    cluster_1.default.on("exit", (worker, code, signal) => {
+        if (code !== 0 && !worker_threads_1.workerData.exitedAfterDisconnect) {
+            console.log(`worker ${worker.id} crashed`);
+            console.log(`Starting new worker`);
+            cluster_1.default.fork();
+        }
+    });
+}
+else {
+    const app = (0, express_1.default)();
+    const port = process.env.PORT || 3000;
+    app.get("/", (req, res) => {
+        console.log("Recordry user microservice");
+        res.send("Recordry user microservice");
+    });
+    app.listen(port, () => { var _a; return console.log(`${(_a = cluster_1.default.worker) === null || _a === void 0 ? void 0 : _a.id} Hello, server running`); });
+}
---
+++ user microservice/src/index.ts
@@ -0,0 +1,36 @@
+import cluster from "cluster";
+import os from "os";
+import { workerData } from "worker_threads";
+
+import express, { Request, Response, Express } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+if (cluster.isMaster) {
+  const cpus = os.cpus().length;
+  console.log(`Forking for ${cpus}`);
+
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    if (code !== 0 && !workerData.exitedAfterDisconnect) {
+      console.log(`worker ${worker.id} crashed`);
+      console.log(`Starting new worker`);
+      cluster.fork();
+    }
+  });
+} else {
+  const app: Express = express();
+  const port = process.env.PORT || 3000;
+
+  app.get("/", (req: Request, res: Response) => {
+    console.log("Recordry user microservice");
+    res.send("Recordry user microservice");
+  });
+
+  app.listen(port, () =>
+    console.log(`${cluster.worker?.id} Hello, server running`)
+  );
+}
